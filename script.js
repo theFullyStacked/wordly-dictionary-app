1bd4082e40f500388f5c0e853e4cc85ee53358dc
William Munene <williammunene2018@gmail.com>
2026-05-01 18:35:14 +0300

    improved functionality

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const input = document.getElementById('word');
 const message = document.getElementById('message');
 const resetButton = document.getElementById('reset-btn');
 
-
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+
+    // clear output before displaying again
+    message.innerHTML='';
+
     const word = input.value;
     if (word.trim() == '') {
         message.textContent = 'Type a word to search.';
@@ -17,14 +20,28 @@ form.addEventListener('submit', function (event) {
         })
         .then(function (data) {
             const entry = data[0];
-            const firstMeaning = entry.meanings[0];
-            const partOfSpeech = firstMeaning.partOfSpeech;
-            const definition = firstMeaning.definitions[0].definition;
+            const phonetic = entry.phonetic;
+            const meaning = entry.meanings[0];
+            const partOfSpeech = meaning.partOfSpeech;
+            const definition = meaning.definitions[0].definition;
 
-            message.innerHTML = `Word: ${entry.word}<br>
-            Part of Speech: ${partOfSpeech}<br>
-            Definition: ${definition}<br>
-            Origin: ${entry.license.name}`;
+            message.innerHTML+=`<h2>${word.toUpperCase()}</h2>`
+            if(entry.phonetic){
+                message.innerHTML+=`${entry.phonetic}<br>`
+            }else if(entry.phonetics[0].text){
+                message.innerHTML+=`${entry.phonetics[0].text}<br>`
+            }
+            message.innerHTML+=`<h3>Definitions</h3>`
+            entry.meanings.forEach(function (meaning) {
+                message.innerHTML += `<i>(${meaning.partOfSpeech})</i><br>`;                
+                meaning.definitions.forEach(function (define) {
+                    message.innerHTML += `<p>${define.definition}</p>`;
+                });
+                message.innerHTML+=`<br><br>`;
+            });
+
+            // clear search bar once content loads
+            input.value = '';
         })
         .catch(function (error) {
             // for words that don't exist in the dictionary
@@ -32,8 +49,9 @@ form.addEventListener('submit', function (event) {
         })
 })
 
-// for clearing the page
+// clear the page
 resetButton.addEventListener('click', () => {
     input.value = '';
     message.textContent = '';
 })
+
